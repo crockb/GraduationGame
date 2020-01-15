fb079f02a9237dbf2f04154b2adf567506069611
Luke <burrisl@oregonstate.edu>
2020-01-14 21:33:11 -0500

    Update app.js

--- Graduation Game Webapp/app.js
+++ Graduation Game Webapp/app.js
@@ -30,5 +30,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://flip1.engr.oregonstate:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
