b5957eac8a3bded64ec98847d18814cae65f2593
holly Switzer <holly12ann@gmail.com>
2020-02-09 20:48:12 -0600

    updating education

--- Graduation Game Project/Assets/LibraryData.cs
+++ Graduation Game Project/Assets/LibraryData.cs
@@ -18,7 +18,6 @@ public class LibraryData : MonoBehaviour
     private LibraryLevel currentLevel;
 
 
-    //1
     // Use this for initialization
     void Start()
     {
@@ -33,56 +32,7 @@ public class LibraryData : MonoBehaviour
 
     void OnEnable()
     {
-        CurrentLevel = levels[0];
-    }
-
-     //1
-    public LibraryLevel CurrentLevel
-    {
-        //2
-        get
-        {
-            return currentLevel;
-        }
-        //3
-        set
-        {
-            currentLevel = value;
-            int currentLevelIndex = levels.IndexOf(currentLevel);
-
-            GameObject levelVisualization = levels[currentLevelIndex].visualization;
-            for (int i = 0; i < levels.Count; i++)
-            {
-                if (levelVisualization != null)
-                {
-                    if (i == currentLevelIndex)
-                    {
-                        levels[i].visualization.SetActive(true);
-                    }
-                    else
-                    {
-                        levels[i].visualization.SetActive(false);
-                    }
-                }
-            }
-        }
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnEnable()
-    {
-        CurrentLevel = levels[0];
+        currentLevel = levels[0];
     }
 
     public LibraryLevel GetNextLevel()
@@ -104,7 +54,7 @@ public class LibraryData : MonoBehaviour
         int currentLevelIndex = levels.IndexOf(currentLevel);
         if (currentLevelIndex < levels.Count - 1)
         {
-            CurrentLevel = levels[currentLevelIndex + 1];
+            currentLevel = levels[currentLevelIndex + 1];
         }
     }
 
