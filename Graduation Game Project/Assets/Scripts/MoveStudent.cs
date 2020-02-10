9274206488eb06e28ce19efa91e2e27039d1a36b
Luke <burrisl@oregonstate.edu>
2020-02-09 19:31:26 -0500

    Updates

---
+++ Graduation Game Project/Assets/Scripts/MoveStudent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStudent : MonoBehaviour
+{
+	public int StartElement = 0;
+	public GameObject[] Waypoints;
+	public float Speed = 1.0f;
+	private float lastWaypointSwitchTime;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startPosition = Waypoints[StartElement].transform.position;
+		Vector3 endPosition = Waypoints[StartElement + 1].transform.position;
+
+		float pathLength = Vector3.Distance (startPosition, endPosition);
+		float totalTimeForPath = pathLength / Speed;
+		float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+		gameObject.transform.position = Vector2.Lerp (startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+
+		if (gameObject.transform.position.Equals(endPosition)) 
+		{
+			if (StartElement < Waypoints.Length - 2)
+			{
+		    	StartElement++;
+		    	lastWaypointSwitchTime = Time.time;
+		    	// TODO: Rotate into move direction
+		  	}
+		  	else
+		  	{
+		    	Destroy(gameObject);
+
+		    	AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+		    	AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+		    	// TODO: deduct health
+		  	}
+		}
+    }
+}
---
+++ Graduation Game Project/Assets/Scripts/SpawnStudent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStudent : MonoBehaviour
+{
+    public GameObject[] Waypoints;
+	public GameObject ObjectToSpawn;
+    public int NumberObjectsToSpawn;
+    public float SpawnFrequencySeconds;
+    public Transform ParentCanvas;
+    
+    private int objectsSpawned = 1; // One is already created
+    private float nextSpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(ObjectToSpawn).GetComponent<MoveStudent>().Waypoints = Waypoints;
+        // This is the time that the next object will be spawned
+        nextSpawnTime = Time.time + SpawnFrequencySeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check to make sure enough time has passed and that we are under the object spawn limit
+        if (Time.time > nextSpawnTime && objectsSpawned < NumberObjectsToSpawn)
+        {
+            var clone = Instantiate(ObjectToSpawn);
+            clone.transform.SetParent(ParentCanvas); // Doing this because of layering issues
+            clone.GetComponent<MoveStudent>().Waypoints = Waypoints;
+            objectsSpawned++;
+            nextSpawnTime += SpawnFrequencySeconds;
+        }
+    }
+}
