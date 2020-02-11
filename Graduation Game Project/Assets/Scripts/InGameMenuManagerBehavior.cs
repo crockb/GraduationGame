1015486b04e8d6e2db8a2fc8e0fd41b0afa677b7
Luke <burrisl@oregonstate.edu>
2020-02-10 21:42:07 -0500

    Update

---
+++ Graduation Game Project/Assets/Scripts/InGameMenuManagerBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameMenuManagerBehavior : MonoBehaviour
+{
+	public Text graduationCoinsLabel;
+	private int graduationCoins;
+
+	public int GraduationCoins {
+	  get
+	  { 
+	    return graduationCoins;
+	  }
+	  set
+	  {
+	    graduationCoins = value;
+	    graduationCoinsLabel.GetComponent<Text>().text = "$" + graduationCoins;
+	  }
+	}
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GraduationCoins = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
