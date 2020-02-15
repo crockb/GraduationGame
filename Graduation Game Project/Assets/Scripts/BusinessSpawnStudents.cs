6cc319b6a37d310eae0f05680b646dbbfa5390af
crockb <crockb@gmail.com>
2020-02-15 13:41:42 -0600

    Adding waves and rouates to the Business levels

--- Graduation Game Project/Assets/Scripts/BusinessSpawnStudents.cs
+++ Graduation Game Project/Assets/Scripts/BusinessSpawnStudents.cs
@@ -2,41 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
+
 [System.Serializable]
 public class Wave
 {
   public GameObject studentPrefab;
   public float spawnInterval = 2;
   public int maxStudents = 20;
+  public GameObject[] waypoints;
 }
-*/
 
 public class BusinessSpawnStudents : MonoBehaviour
 {
-/*
+
 	public Wave[] waves;
 	public int timeBetweenWaves = 5;
+
 	private InGameMenuManagerBehavior gameManager;
+
 	private float lastSpawnTime;
-	private int studentsSpawned;
-*/
+	private int studentsSpawned = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    	lastSpawnTime = Time.time;
+    	gameManager = GameObject.Find("InGameMenuManager").GetComponent<InGameMenuManagerBehavior>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// 1
+		int currentWave = gameManager.Wave;
+		if (currentWave < waves.Length)
+		{
+		  // 2
+		  float timeInterval = Time.time - lastSpawnTime;
+		  float spawnInterval = waves[currentWave].spawnInterval;
+		  if (((studentsSpawned == 0 && timeInterval > timeBetweenWaves) ||
+		       timeInterval > spawnInterval) && 
+		      studentsSpawned < waves[currentWave].maxStudents)
+		  {
+		    // 3  
+		    lastSpawnTime = Time.time;
+		    GameObject newStudent = (GameObject)
+		        Instantiate(waves[currentWave].studentPrefab);
+
+    		newStudent.GetComponent<BusinessMoveStudents>().waypoints = waves[currentWave].waypoints;
+		    studentsSpawned++;
+		  }
+		  // 4 
+		  if (studentsSpawned == waves[currentWave].maxStudents &&
+		      GameObject.FindGameObjectWithTag("Student") == null)
+		  {
+		    gameManager.Wave++;
+		    //gameManager.Gold = Mathf.RoundToInt(gameManager.Gold * 1.1f);
+		    studentsSpawned = 0;
+		    lastSpawnTime = Time.time;
+
+		  }
+		  // 5 
+		}
+		else
+		{
+		  //gameManager.gameOver = true;
+		  //GameObject gameOverText = GameObject.FindGameObjectWithTag ("GameWon");
+		  //gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+		}
+
+    }
+}
+
+
+/* WORKING FOR 1 STUDUENT TO SPAWN 
+
+public class BusinessSpawnStudents : MonoBehaviour
+{
+
 
 	public GameObject[] waypoints;
-	public GameObject testStudentPrefab;
+	//public GameObject testStudentPrefab;
+
+	public GameObject studentPrefab;
 	private int i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
 
-    	/*
-    	lastSpawnTime = Time.time;
-    	gameManager = GameObject.Find("GameManager").GetComponent<InGameMenuManagerBehavior>();
-		
-		*/
+    	//testStudentPrefab = GameObject.Find("UnderGrad(DEV)");
 
-    	testStudentPrefab = GameObject.Find("UnderGrad(DEV)");
+    	GameObject newStudent = (GameObject)
+    		Instantiate(studentPrefab);
+
+    	// set the waypoints
     	waypoints = new GameObject[12];
     	foreach(Transform child in transform)
     	{
@@ -44,14 +106,9 @@ public class BusinessSpawnStudents : MonoBehaviour
     		i++;
     	}
 
-        Instantiate(testStudentPrefab).GetComponent<BusinessMoveStudents>().waypoints = waypoints;
+    	newStudent.GetComponent<BusinessMoveStudents>().waypoints = waypoints;
 
-
+        //Instantiate(testStudentPrefab).GetComponent<BusinessMoveStudents>().waypoints = waypoints;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+ */
