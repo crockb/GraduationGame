ff1867bc3de84e10136949f090537e75bc50e85c
crockb <crockb@gmail.com>
2020-02-19 21:45:16 -0600

    Updates to HealthBar and destroying Students
    Updated the "HealthBar" script and Student Prefabs to destroy the Student once health reaches 0.  Fixed the Healthbar to resize towards the left.

--- Graduation Game Project/Assets/Scripts/HealthBar.cs
+++ Graduation Game Project/Assets/Scripts/HealthBar.cs
@@ -7,28 +7,42 @@ public class HealthBar : MonoBehaviour
     public float maxHealth = 100;
     public float currentHealth = 100;
     private float originalScale;
+    private GameObject theHealthBar;
     private float nextActionTime = 0.0f;
     public float healthDeclineSeconds;
+    private InGameMenuManagerBehavior gameManager;
 
 
     // Use this for initialization
     void Start()
     {
-        originalScale = gameObject.transform.localScale.x;
+        theHealthBar = gameObject.transform.Find("HealthBar").gameObject;
+        originalScale = theHealthBar.transform.localScale.x;
+        gameManager = GameObject.Find("InGameMenuManager").GetComponent<InGameMenuManagerBehavior>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Time.time > nextActionTime ) 
+        if (Time.time > nextActionTime && currentHealth > 0) 
         { 
             nextActionTime = Time.time + healthDeclineSeconds;
             currentHealth = currentHealth - 1;
         } 
 
-        Vector3 tmpScale = gameObject.transform.localScale;
+        if (currentHealth == 0)
+        {
+            //AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            //audioSource.PlayOneShot(audioSource.clip);
+            Destroy(gameObject);
+            gameManager.DropOuts = GameStats.dropouts + 1;
+
+        }
+
+        Vector3 tmpScale = theHealthBar.transform.localScale;
         tmpScale.x = currentHealth / maxHealth * originalScale;
-        gameObject.transform.localScale = tmpScale;
+        theHealthBar.transform.localScale = tmpScale;
     }
 }
